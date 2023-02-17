026aab1ac4f72f228de081208e18880cb68b8d3c
patrickwebdev <57304731+patrickwebdev@users.noreply.github.com>
2023-02-17 17:05:54 -0500

    Update class11.php

--- demonstrations/class11/class11.php
+++ demonstrations/class11/class11.php
@@ -1,44 +1,44 @@
 <?php 
+//Create a Class
 class Students
 {
+  //Declare Properties
   public $first_name;
   public $last_name;
-  public $id;
+  private $id;
   
- 
+  //Create a Method
   public function getFirstName()
   {
     return $this->first_name;
   }
  
+  //Create a Method
   public function getLastName()
   {
     return $this->last_name;
   }
 
+  //Create a Method
   private function setId()
   {
-    $id=rand();
-    return $id; 
+    $this->id=rand();
   }
  
+  //Create a Method
   public function getId()
   {
-    return $this->setId(); 
-  }
-
-  public function save_user(){
-    echo "Your profile have been saved <br/>";
-    echo "Fisrt Name : " . $this->getFirstName();
+    $this->setId();
+    return $this->id; 
   }
 }
 
-//Instantiate objects
+//Instantiate Objects
 $student1 = new Students;
 $student2 = new Students;
 $student3 = new Students;
 
-//Assign value to properties
+//Assign value to Properties
 $student1->first_name = 'Patrick';
 $student1->last_name = 'Saint-Louis';
 
@@ -49,7 +49,7 @@ $student3->first_name = 'Albert';
 $student3->last_name = 'Einstein'; 
 
  
-//Display outputs from methods
+//Display outputs from Methods
 echo"<pre>";
 
 echo"<p>First Name:".$student1->getFirstName()."</p>";
